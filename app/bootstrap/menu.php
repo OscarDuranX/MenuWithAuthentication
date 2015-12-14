3e4a6cb71b9a7a85405cae48b8d3f6e3e98582f7
OscarDuranX <oscarduran@iesebre.com>
2015-12-14 16:55:19 +0100

    Proves amb el bootstrap

--- app/bootstrap/menu.php
+++ app/bootstrap/menu.php
@@ -1,9 +1,24 @@
 <?php
 //Home
-MenuWithAuthentication::
+MenuWithAuthentication::menu()
+    -> title('Home')
+    ->icon('fa-dashboard')
+    ->url('/home')
+    ->rol('home')
+    ->permission('home')
+    ->user('Oscar');
 
 //Another Links
-MenuWithAuthentication::
+MenuWithAuthentication::menu()
+    -> title('Another links')
+    ->user(5);
 
 //Multilevel
-MenuWithAuthentication::
+MenuWithAuthentication::menu() -> title('Multilevel')->icon('fa-battery-full');
+    MenuWithAuthentication::menu() -> title('Link in lvl 1');
+    MenuWithAuthentication::menu() -> title('Link in lvl 2');
+
+MenuWithAuthentication::menu()
+    -> title('Últim menú')
+    ->icon('fa-briefcase')
+    ->url('https://www.google.es/');
